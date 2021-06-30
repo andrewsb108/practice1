fed38b9d66ba215853bf4ce617da17ffdb9714fd
andrewsb108 <andrey.barsuk1@gmail.com>
2021-06-30 13:18:31 +0300

    Fixed gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         Model model = new Model();
         View view = new View();
         Controller controller = new Controller(model, view);
